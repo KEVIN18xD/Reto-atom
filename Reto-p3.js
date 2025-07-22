fd3f279cfe3e6edd9166b0d51bf536538d81821a
Kevin Raxcacó <93635504+KEVIN18xD@users.noreply.github.com>
2025-07-22 13:18:53 -0600

    Add files via upload

---
+++ Reto-p3.js
@@ -0,0 +1,21 @@
+## selector Tipo
+{{ 
+  _.uniq(GetVehiculos.data.records
+    .map(r => r.fields.Tipo)
+    .filter(Boolean) // elimina null o undefined
+  ).map(t => ({ label: t, value: t }))
+}}
+
+## Selector Marca
+{{ 
+  _.uniq(GetVehiculos.data.records
+    .map(r => r.fields.Marca)
+    .filter(Boolean)
+  ).map(m => ({ label: m, value: m }))
+}}
+
+## Datos tabla
+{{ BuscarFiltrados.data.records.map(r => ({
+  ID: r.id,
+  ...r.fields
+})) }}
